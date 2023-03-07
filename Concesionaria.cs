38234bc8e07f6d2f8a2f12f1aa02c6b103c51794
Petersalcedo <pitersalcedo87@gmail.com>
2023-03-07 15:55:18 -0600

    [Trabajo 2]

---
+++ Concesionaria.cs
@@ -0,0 +1,23 @@
+namespace Ejercicio2;
+class Automovil
+{
+    public List<string> lista;
+    static void Main(string[] args) 
+    {
+        lista.add(1);
+        foreach (string item in lista)
+        {
+            Console.WriteLine(item);
+        }  
+        a a= new a();
+        Console.WriteLine(a.ToString{});
+    }
+}
+
+class a{
+    int prop;
+    public override.string.ToString()
+    {
+        return base.ToString();
+    }
+}
